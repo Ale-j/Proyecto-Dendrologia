01a01b4e18784f1797d38f9047d7b447ed83dd7e
ccganant <boldseis@gmail.com>
2019-11-03 11:14:28 -0500

    Propuesta de presentacion

---
+++ dendroapp.R
@@ -0,0 +1,22 @@
+
+library(shiny)
+
+datos<- read.csv("dendro_parcial1.csv", encoding = "UTF-8")
+
+ui<- fluidPage(
+  selectInput(inputId = "familia", 
+              label = strong("Seleccione una familia:"), 
+              choices = unique(datos$familia),
+              selected = "fabaceae"),
+  dataTableOutput(outputId = "info"))
+
+server<- function(input, output) {
+  
+  sel<- reactive(datos %>% filter(familia == input$familia))
+  output$info<- renderDataTable({
+    sel()
+  }, options = list(aLengthMenu = c(5, 10, 20), iDisplayLength = 5))
+  
+}
+
+shinyApp(ui = ui, server= server)
